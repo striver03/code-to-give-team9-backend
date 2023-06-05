a0018a96c0592f34675be51b49150a6a8edb63bc
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2023-06-06 00:55:28 +0530

    added Create and Read for form

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 // Initial setup
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Middleware
 app.use(express.json());
 
-const crudRouter = require('./routes/crud');
-app.use('/',crudRouter);
+// const crudRouter = require('./routes/crud');
+const formRouter = require("./routes/form");
+app.use("/form", formRouter);
+// app.use('/', crudRouter);
 
 const port = process.env.PORT || 3000;
 
 const start = () => {
-    try {
-        app.listen(3000, console.log(`Server is listening on the port ${port}...`));
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    app.listen(port, console.log(`Server is listening on the port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
 };
 start();
 
 // Collections:
 // 1. users (the complete data of each user)
-//     docs - 
+//     docs -
 // 2. age bracket - (below 18, 18-20, 21-23, 24+ {these will be the documents of the collection})
-   
---
+++ models/form.js
@@ -0,0 +1,92 @@
+const db = require("../db/connect");
+const { FormDoesNotExistError } = require("./errors/form");
+
+/**
+Model for the form object
+*/
+class Form {
+  /**
+  Constructor for the form object
+
+  @typedef {Object} Form
+  @property {string} name - The name of the form
+  @property {string} createdBy - The user who created the form
+  @property {string} id - The id of the form
+
+  @param {Form} form - The form object
+  */
+  constructor({ name, createdBy, id = null }) {
+    if (name == null || name === "") {
+      throw new Error("Form name is undefined");
+    }
+
+    if (createdBy == null || createdBy === "") {
+      throw new Error("Form createdBy is undefined");
+    }
+
+    this.name = name;
+    this.createdBy = createdBy;
+    this.id = id;
+  }
+
+  /**
+  Returns a JSON representation of the form
+  @return {Object} - The JSON representation of the form
+  */
+  json() {
+    return {
+      name: this.name,
+      createdBy: this.createdBy,
+      id: this.id,
+    };
+  }
+}
+
+class FormDao {
+  /**
+  Fetches a form from the database
+  @param {string} id - The id of the form to fetch
+  @return {Promise<Form>} - The form object
+
+  @throws {Error} - If the form does not exist
+  */
+  static async getForm(id) {
+    const formRef = db.collection("forms").doc(id);
+    const formDoc = await formRef.get();
+
+    if (!formDoc.exists) {
+      throw new FormDoesNotExistError();
+    }
+
+    const form = new Form({
+      name: formDoc.data().name,
+      createdBy: formDoc.data().createdBy,
+      id: formDoc.id,
+    });
+
+    return form;
+  }
+
+  /**
+  Creates a form in the database
+  @param {Form} form - The form to create
+
+  @return {Promise<Form>} - The form object
+  */
+  static async createForm(form) {
+    const formsRef = db.collection("forms");
+
+    const setForm = await formsRef.add({
+      name: form.name,
+      createdBy: form.createdBy,
+    });
+
+    return new Form({
+      name: form.name,
+      createdBy: form.createdBy,
+      id: setForm.id,
+    });
+  }
+}
+
+module.exports = { Form, FormDao };
