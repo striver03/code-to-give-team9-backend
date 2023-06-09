b3d5adc0786dc343adb86b1a6fa0de0a6e1683cd
Rishabh Prasad <rishabh.prasad2003@gmail.com>
2023-06-09 17:11:51 +0530

    GPT route set to controller

--- routes/gpt.js
+++ routes/gpt.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// const {getnextQues} = require('../controllers/gpt');
-const {getnextQues} = require('../archive/gpt2');
+const {getnextQues} = require('../controllers/gpt');
 
 // router.route('/base').post(storeBaseInfo);
 router.route('/').post(getnextQues);
