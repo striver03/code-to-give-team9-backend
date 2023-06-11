bd4d8156e302bee4cd7ece72bfca5072ed046294
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2023-06-12 02:57:28 +0530

    fixed /counselling

--- controllers/counselling.js
+++ controllers/counselling.js
@@ -1,27 +1,44 @@
 import db from "../db/connect.js";
-const counsellingBookedCollection = db.collection('counselling-booked');
+const counsellingBookedCollection = db.collection("counselling-booked");
 
-const getCounsellingData = async(req,res)=>{
-    counsellingBookedCollection
+const nextIdFromRef = (ref) => {
+  if (typeof ref === "string") {
+    return ref;
+  }
+  let segments = ref._path.segments;
+  let newNext = segments[segments.length - 1];
+
+  return newNext;
+};
+
+const getCounsellingData = async (req, res) => {
+  counsellingBookedCollection
     .get()
     .then(async (querySnapshot) => {
-        const docs = querySnapshot.docs.map(doc => doc.data());
-        const data = [];
-        for(let idx in docs)
-        {
-          try {
-            let formSumissionId = docs[idx].submission;
-            const formSubmissionDocRef = db.collection("form-submissions").doc(formSumissionId);
-            const snapshot = await formSubmissionDocRef.get();
-            data.push(snapshot.data());
-          } catch (error) {
-            res.send("failed with error: " + error);         
-          }
-        }
-        res.status(200).json({data});
-      }).catch((error)=>{
-        res.send("failed with error: " + error);
-      });
-}
+      const docs = querySnapshot.docs.map((doc) => doc.data());
+      const data = [];
+      for (let idx in docs) {
+        console.log(docs[idx].submission);
 
-export {getCounsellingData};
+        let formSubmissionId =
+          typeof docs[idx].submission === "string"
+            ? docs[idx].submission
+            : nextIdFromRef(docs[idx].submission);
+
+        if (formSubmissionId == null) continue;
+
+        const formSubmissionDocRef = db
+          .collection("form-submissions")
+          .doc(formSubmissionId);
+        const snapshot = await formSubmissionDocRef.get();
+        data.push(snapshot.data());
+      }
+      res.status(200).json({ data: data.filter((d) => d != null) });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.send("failed with error: " + error);
+    });
+};
+
+export { getCounsellingData };
